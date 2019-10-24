91fd376f2ef96887f09c670e9b694d380e5d55c4
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-10-24 14:36:05 +0300

    Fix resource saving w/ notes field

--- resources/js/notes-field/FormField.vue
+++ resources/js/notes-field/FormField.vue
@@ -10,5 +10,10 @@ export default {
   props: ['field', 'resourceId', 'resourceName'],
   components: { NotesField },
   mixins: [HandlesValidationErrors, FormField, InteractsWithDates],
+  methods: {
+    fill(formData) {
+      // Do nothing, as this is an invisible field
+    },
+  },
 };
 </script>
