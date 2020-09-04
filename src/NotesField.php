64db4d8d5d4484267c1559e4ad81d5bc7dd4a0f5
SimonBarrettACT <simonbarrett@acttraining.org.uk>
2020-09-04 09:56:18 +0100

    Added full width and visibility options

--- src/NotesField.php
+++ src/NotesField.php
@@ -10,14 +10,70 @@ class NotesField extends Field
     public $showOnCreation = false;
     public $showOnIndex = false;
 
+    public $inputWidth = 'w-3/5';
+    public $showAddNote = true;
+    public $showNotes = true;
+
+    /**
+     * NovaNotesField constructor.
+     *
+     * @param $name
+     * @param null $attribute
+     * @param callable|null $resolveCallback
+     */
+    public function __construct($name, $attribute = null, callable $resolveCallback = null)
+    {
+        parent::__construct($name, $attribute, $resolveCallback);
+
+        $this->withMeta([
+            'inputWidth' => $this->inputWidth,
+            'showAddNote' => $this->showAddNote,
+            'showNotes' => $this->showNotes
+        ]);
+    }
+
     /**
      * Sets the placeholder value displayed on the field.
      *
      * @param string $placeholder
-     * @return \OptimistDigital\NovaNotesField\NotesField
+     * @return NotesField
      **/
     public function placeholder($placeholder)
     {
         return $this->withMeta(['placeholder' => $placeholder]);
     }
+
+    /**
+     * Sets the input size.
+     *
+     * @param string $inputWidth
+     * @return NotesField
+     **/
+    public function inputWidth($inputWidth)
+    {
+        return $this->withMeta(['inputWidth' => $inputWidth]);
+    }
+
+    /**
+     * Show or hide the AddNote input.
+     *
+     * @param string $showAddNote
+     * @return NotesField
+     **/
+    public function showAddNote($showAddNote)
+    {
+        return $this->withMeta(['showAddNote' => $showAddNote]);
+    }
+
+    /**
+     * Show or hide the notes section
+     *
+     * @param string $showNotes
+     * @return NotesField
+     **/
+    public function showNotes($showNotes)
+    {
+        return $this->withMeta(['showNotes' => $showNotes]);
+    }
+    
 }
