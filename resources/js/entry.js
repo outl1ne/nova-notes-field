93bcf695d851441dffd3306b0343fe0a9c1f55fe
Tarvo R <tarvoreinpalu@gmail.com>
2023-02-23 10:11:38 +0200

    Fix dark mode

--- resources/js/entry.js
+++ resources/js/entry.js
@@ -2,17 +2,20 @@ import IndexField from './notes-field/IndexField';
 import DetailField from './notes-field/DetailField';
 import FormField from './notes-field/FormField';
 
-Nova.booting((Vue, router, store) => {
-  new MutationObserver(() => {
-    const cls = document.documentElement.classList;
-    const isDarkMode = cls.contains('dark');
+const handleDarkMode = () => {
+  const cls = document.documentElement.classList;
+  const isDarkMode = cls.contains('dark');
 
-    if (isDarkMode && !cls.contains('o1-dark')) {
-      cls.add('o1-dark');
-    } else if (!isDarkMode && cls.contains('o1-dark')) {
-      cls.remove('o1-dark');
-    }
-  }).observe(document.documentElement, {
+  if (isDarkMode && !cls.contains('o1-dark')) {
+    cls.add('o1-dark');
+  } else if (!isDarkMode && cls.contains('o1-dark')) {
+    cls.remove('o1-dark');
+  }
+};
+
+Nova.booting((Vue, router, store) => {
+  handleDarkMode();
+  new MutationObserver(handleDarkMode).observe(document.documentElement, {
     attributes: true,
     attributeOldValue: true,
     attributeFilter: ['class'],
